0a5b037685a225ef44fc79bd12836c6feab6c4ed
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-05-29 21:15:40 +0900

    initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $users = User::all();
+        return response()->json($users, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $user = User::create($request->all());
+        return response()->json($user, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+        $user = User::find($id);
+        return response()->json($user, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+        $user = User::find($id);
+        $user->update($request->all());
+        return response()->json($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+        $user = User::find($id);
+        $user->delete();
+        return response()->json(null, 204);
+    }
+}
